568edb1d6fa08e7cf7eeb2093fa5b3e11a9e132a
mottirioisii <mottirioisii@gmail.com>
2017-12-01 21:04:30 +0900

    オオオ

--- object_detection/clothes_app.py
+++ object_detection/clothes_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from fashion_detection_tutorial import fashion_detector, image_load
+import requests
 
 app = Flask(__name__)
 
@@ -11,12 +12,22 @@ def cloth_detection_facet():
     Returns:
 
     """
-    path = request.args.get("image_path")
-    print(path)
+    image_name = request.form["image_path"]
+    path = '/home/hashimoto/LineBot' + image_name
     image = image_load(path=path)
     file_name = path.split('/')
-    cropped_dir_name = '/home/hashimoto/LineBot/cropped/'
-    return fashion_detector(pil_image=image, output_name=cropped_dir_name + file_name[-1])
+    cropped_dir_name = '/home/hashimoto/LineBot/tmp/cropped'
+    output_name = cropped_dir_name + file_name[-1]
+
+    rslt = fashion_detector(pil_image=image, output_name=output_name)
+    if rslt:
+        header = {'content-type': 'application/json'}
+        print(requests.post(url='http://127.0.0.1:9999/resize',
+                            headers=header,
+                            data="{'image_path':'" + output_name + "'}"))
+    else:
+        print('failed detect')
+    return
 
 
 if __name__ == '__main__':
